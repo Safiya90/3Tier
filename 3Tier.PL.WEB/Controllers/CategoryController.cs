42d143d76b80dac06f9f34063ba0acd0ae96f623
safiya Khamis <safia5000@hotmail.com>
2025-06-17 20:18:15 +0400

    Identity commit

---
+++ 3Tier.PL.WEB/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3Tier.PL.WEB.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 3Tier.PL.WEB/Controllers/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3Tier.PL.WEB.Controllers
+{
+    public class IGenericRepository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
