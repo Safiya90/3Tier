068f660c554d74afafc8ef23fe3350b83dc60b08
safiya Khamis <safia5000@hotmail.com>
2025-06-17 18:41:13 +0400

    GenericRepositry

---
+++ 3Tier.BLL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Tier.BLL.Interfaces
+{
+    internal class IGenericRepository
+    {
+    }
+}
